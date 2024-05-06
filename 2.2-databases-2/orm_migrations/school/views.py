1352965433c2c7066f17113334f557a7c61fd219
Diana <d.dyulberova@gmail.com>
2024-05-06 22:12:24 +0500

    HW_4

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -4,12 +4,17 @@ from django.shortcuts import render
 from .models import Student
 
 
+# def students_list(request):
+#     template = 'school/students_list.html'
+#     ordering = 'group'
+#     students_list = Student.objects.all().order_by(ordering).prefetch_related('teachers')
+#     context = {'object_list': students_list}
+#     return render(request, template, context)
+
+
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-
+    students_list = Student.objects.all().order_by(ordering).prefetch_related('teachers')
+    context = {'object_list': students_list}
     return render(request, template, context)
